bea4e16ea39b378e29ba2bc8268cd7de92e509a9
Bruno Kambere <brunokambere@gmail.com>
2024-01-21 23:37:20 +0200

    Refactor SocialIcon component, add prop types and fix linters

--- src/components/home/SocialIcon.js
+++ src/components/home/SocialIcon.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function SocialIcon(props) {
-    const {link, icon, label} = props;
-    return (
-        <a target="_blank" aria-label={label}
-           rel="noopener noreferrer" href={link}>
-            <i className={icon} aria-hidden="true"/>
-        </a>
-    );
+  const { link, icon, label } = props;
+  return (
+    <a
+      target="_blank"
+      aria-label={label}
+      rel="noopener noreferrer"
+      href={link}
+    >
+      <i className={icon} aria-hidden="true" />
+    </a>
+  );
 }
+
+SocialIcon.propTypes = {
+  link: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+};
 
 export default SocialIcon;
