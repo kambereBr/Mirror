1246e4dc9a0eff5305e3e6f33e9f8492b3955648
Bruno Kambere <brunokambere@gmail.com>
2024-01-21 23:46:50 +0200

    Refactor EmojiBullet component and fix linters

--- src/components/home/EmojiBullet.js
+++ src/components/home/EmojiBullet.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import {Box} from "@mui/material";
+import { Box } from '@mui/material';
+import PropTypes from 'prop-types';
 
 function EmojiBullet(props) {
-    const {emoji, text} = props;
+  const { emoji, text } = props;
 
-    return (
-        <Box component={'li'} fontSize={'1rem'} lineHeight={1.5} style={{cursor: 'default'}}>
-            <Box component={'span'} aria-label="cheese"
-                 role="img"
-                 mr={{xs: '0.5rem', md: '1rem'}} fontSize={'1.5rem'}>{emoji}</Box> {text}
-        </Box>
-    );
+  return (
+    <Box component="li" fontSize="1rem" lineHeight={1.5} style={{ cursor: 'default' }}>
+      <Box
+        component="span"
+        aria-label="cheese"
+        role="img"
+        mr={{ xs: '0.5rem', md: '1rem' }}
+        fontSize="1.5rem"
+      >
+        {emoji}
+      </Box>
+      {' '}
+      {text}
+    </Box>
+  );
 }
+
+EmojiBullet.propTypes = {
+  emoji: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
 
 export default EmojiBullet;
