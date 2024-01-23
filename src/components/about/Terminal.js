cb5819997048fd6b63ee7e98d6a304a972ee6433
Bruno Kambere <brunokambere@gmail.com>
2024-01-23 23:46:59 +0200

    Refactor Terminal component styling

--- src/components/about/Terminal.js
+++ src/components/about/Terminal.js
@@ -1,28 +1,48 @@
 import React from 'react';
-import Style from "./Terminal.module.scss";
-import classNames from "classnames";
-import {Box} from "@mui/material";
+import classNames from 'classnames';
+import { Box } from '@mui/material';
+import PropTypes from 'prop-types';
+import Style from './Terminal.module.scss';
 
-const iconClass = "fa fa-circle";
+const iconClass = 'fa fa-circle';
 
 function Terminal(props) {
-   const {text} = props;
+  const { text } = props;
 
-   return (
-      <Box component={'section'} className={classNames(Style.terminal, Style.shadowed)}
-           width={{xs: '80%', md: '50%'}} borderRadius={'0.5rem'} mb={'4rem'}>
-         <Box sx={{backgroundColor: '#8c8c8c'}} p={'0.5rem'} borderRadius={'0.5rem 0.5rem 0 0'}
-              fontSize={'1rem'}>
-            <i className={classNames(iconClass, Style.red)}/>
-            <i className={classNames(iconClass, Style.amber)}/>
-            <i className={classNames(iconClass, Style.green)}/>
-         </Box>
-         <Box py={{xs: '1rem', md: '2rem'}} px={{xs: '2rem', md: '3rem'}} borderRadius={'0 0 0.5rem 0.5rem'}
-              sx={{backgroundColor: '#27242f'}} fontSize={'1.5rem'} fontFamily={'Courier New, Courier, monospace'}>
-            {text}
-         </Box>
+  return (
+    <Box
+      component="section"
+      className={classNames(Style.terminal, Style.shadowed)}
+      width={{ xs: '80%', md: '50%' }}
+      borderRadius="0.5rem"
+      mb="4rem"
+    >
+      <Box
+        sx={{ backgroundColor: '#8c8c8c' }}
+        p="0.5rem"
+        borderRadius="0.5rem 0.5rem 0 0"
+        fontSize="1rem"
+      >
+        <i className={classNames(iconClass, Style.red)} />
+        <i className={classNames(iconClass, Style.amber)} />
+        <i className={classNames(iconClass, Style.green)} />
       </Box>
-   );
+      <Box
+        py={{ xs: '1rem', md: '2rem' }}
+        px={{ xs: '2rem', md: '3rem' }}
+        borderRadius="0 0 0.5rem 0.5rem"
+        sx={{ backgroundColor: '#27242f' }}
+        fontSize="1.5rem"
+        fontFamily="Courier New, Courier, monospace"
+      >
+        {text}
+      </Box>
+    </Box>
+  );
 }
+
+Terminal.propTypes = {
+  text: PropTypes.string.isRequired,
+};
 
 export default Terminal;
